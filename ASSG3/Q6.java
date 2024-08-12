9825dba76de39df15c48f164af7e967b087e9b34
arunnats <arun_b220019cs@nitc.ac.in>
2024-08-12 23:06:09 +0530

    started q6

--- ASSG3/Q6.java
+++ ASSG3/Q6.java
@@ -1,3 +1,56 @@
+import java.util.*;
+
 public class Q6 {
   
+}
+
+class University {
+}
+
+class Department {
+}
+
+
+class Professor {
+  private String name;
+  private String id;
+  private Department department;
+
+  public Professor(String name, String id, Department department) {
+      this.name = name;
+      this.id = id;
+      this.department = department;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public String getId() {
+      return id;
+  }
+}
+
+class Student {
+  private String name;
+  private String id;
+  private String major;
+
+  public Student(String name, String id, String major) {
+      this.name = name;
+      this.id = id;
+      this.major = major;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public String getId() {
+      return id;
+  }
+
+  public String getMajor() {
+      return major;
+  }
 }
